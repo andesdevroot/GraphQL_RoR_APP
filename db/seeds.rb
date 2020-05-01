231b094ee768ada63b8c9a359c93efe316ed1d0d
andesdevroot <andes.dev.root@gmail.com>
2020-05-01 09:57:48 -0400

    se completa archivo seed con datos faker para titulos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,18 @@
 
 PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
 
-puts PROJECT_TITLES
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+  'a@a.com' => PROJECT_TITLES[0, OFFSET],
+  'x@x.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+   user = User.new(email: email, password: 'secret')
+   projects.each { |title| user.projects.build(title: title) }
+   user.save.
+ end
+
+ 
